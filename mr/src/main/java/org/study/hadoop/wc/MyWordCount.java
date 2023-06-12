b10709146e80204014bd25dbcae507335140755f
he.wang <he.wang@yamu.com>
2023-06-12 23:33:08 +0800

    配置本地mr的配置：配置用户名，配置windows通用，配置上传的jar包

--- mr/src/main/java/org/study/hadoop/wc/MyWordCount.java
+++ mr/src/main/java/org/study/hadoop/wc/MyWordCount.java
@@ -16,12 +16,15 @@ public class MyWordCount {
         System.setProperty("HADOOP_USER_NAME", "root");
         Configuration configuration = new Configuration();
 
+        // 【重点】：让框架知道是windows异构平台
+        configuration.set("mapreduce.app-submission.cross-platform", "true");
         // Specify various job-specific parameters
         Job job = Job.getInstance(configuration);
+        // 【重点】：设置jar包路径，当程序运行时，将会上传该jar包
+        job.setJar("D:\\workspace\\codesource\\java\\hadoop-study\\mr\\target\\mr-1.0.jar");
         TextInputFormat.addInputPath(job, new Path("/data/wc/input"));
-
-        TextOutputFormat.setOutputPath(job, new Path("/data/wc/output2"));
-
+        // 【重点】：每次注意，需要换一个路径
+        TextOutputFormat.setOutputPath(job, new Path("/data/wc/output4"));
         job.setJarByClass(MyWordCount.class);
         job.setJobName("MyWordCount");
         job.setMapperClass(MyMapper.class);
